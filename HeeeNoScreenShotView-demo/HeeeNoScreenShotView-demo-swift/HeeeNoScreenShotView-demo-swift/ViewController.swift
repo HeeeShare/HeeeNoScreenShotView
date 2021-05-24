6f2af6650ad100abec32f3ef23c150c38abd2ea3
Heee <zhengjuan@danlu.com>
2021-05-24 13:09:52 +0800

    demo

--- HeeeNoScreenShotView-demo/HeeeNoScreenShotView-demo-swift/HeeeNoScreenShotView-demo-swift/ViewController.swift
+++ HeeeNoScreenShotView-demo/HeeeNoScreenShotView-demo-swift/HeeeNoScreenShotView-demo-swift/ViewController.swift
@@ -12,25 +12,27 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let noScreenShotView = HeeeNoScreenShotView.init(frame: .init(x: 50, y: 50, width: 240, height: 200))
-    noScreenShotView.backgroundColor = .init(white: 0.95, alpha: 1.0)
-    self.view.addSubview(noScreenShotView)
-    
-    let privacyBtn = UIButton.init(frame: .init(x: 10, y: 30, width: 100, height: 40))
-    privacyBtn.backgroundColor = .systemRed
-    privacyBtn.setTitle("隐私按钮", for: .normal)
-    privacyBtn.setTitleColor(.white, for: .normal)
-    privacyBtn.titleLabel?.font = .systemFont(ofSize: 15)
-    privacyBtn.addTarget(self, action: #selector(click), for: .touchUpInside)
-    noScreenShotView.addSubview(privacyBtn)
-    
-    let privacyLabel = UILabel.init(frame: .init(x: 130, y: 30, width: 100, height: 40))
-    privacyLabel.textAlignment = .center
-    privacyLabel.backgroundColor = .systemGreen
-    privacyLabel.textColor = .black
-    privacyLabel.font = .systemFont(ofSize: 15)
-    privacyLabel.text = "隐私文字"
-    noScreenShotView.addSubview(privacyLabel)
+    if #available(iOS 13.0, *) {
+      let noScreenShotView = HeeeNoScreenShotView.init(frame: .init(x: 50, y: 50, width: 240, height: 200))
+      noScreenShotView.backgroundColor = .init(white: 0.95, alpha: 1.0)
+      self.view.addSubview(noScreenShotView)
+      
+      let privacyBtn = UIButton.init(frame: .init(x: 10, y: 30, width: 100, height: 40))
+      privacyBtn.backgroundColor = .systemRed
+      privacyBtn.setTitle("隐私按钮", for: .normal)
+      privacyBtn.setTitleColor(.white, for: .normal)
+      privacyBtn.titleLabel?.font = .systemFont(ofSize: 15)
+      privacyBtn.addTarget(self, action: #selector(click), for: .touchUpInside)
+      noScreenShotView.addSubview(privacyBtn)
+      
+      let privacyLabel = UILabel.init(frame: .init(x: 130, y: 30, width: 100, height: 40))
+      privacyLabel.textAlignment = .center
+      privacyLabel.backgroundColor = .systemGreen
+      privacyLabel.textColor = .black
+      privacyLabel.font = .systemFont(ofSize: 15)
+      privacyLabel.text = "隐私文字"
+      noScreenShotView.addSubview(privacyLabel)
+    }
   }
   
   @objc func click() {
